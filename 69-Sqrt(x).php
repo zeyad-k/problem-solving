b5ca01a08e7fd6eff23e87dc0a5ca3fa7d9d7d05
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-10-13 14:26:45 +0300

    [69] [Time Beats: 78.33%] [Memory Beats: 37.93%] - LeetPush

--- 69-Sqrt(x).php
+++ 69-Sqrt(x).php
@@ -5,17 +5,26 @@ class Solution {
      * @return Integer
      */
     function mySqrt($x) {
-        if($x==0 || $x==1)
-        {
+        if ($x == 0 || $x == 1) {
             return $x;
         }
-        $result=1;
-        $i=1;
-        while($result <= $x){
-            $i++;
-            $result=$i*$i;
+
+        $left = 1;
+        $right = $x;
+
+        while ($left <= $right) {
+            $mid = $left + intval(($right - $left) / 2);
+            $square = $mid * $mid;
+
+            if ($square == $x) {
+                return $mid;
+            } elseif ($square < $x) {
+                $left = $mid + 1;
+            } else {
+                $right = $mid - 1;
+            }
         }
-        return $i-1;
-        
+
+        return $right; // $right will be the largest integer whose square is <= $x
     }
 }
