4b92621f446db8d0b925f71601bffafca046f1e9
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-12-28 22:21:45 +0200

    [1929] [Time Beats: 100.00%] [Memory Beats: 10.26%] - LeetPush

---
+++ 1929-Concatenation-of-Array.php
@@ -0,0 +1,11 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer[]
+     */
+   function getConcatenation($nums) {
+        // return array_merge($nums, $nums);
+         return [...$nums, ...$nums];
+    }
+}
