cd3a5fcd0824b6fe02322f30b71c47d9778678e7
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-12-31 23:37:59 +0200

    [2278] [Time Beats: 100.00%] [Memory Beats: 49.40%] - LeetPush

---
+++ 2278-Percentage-of-Letter-in-String.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {character} letter
+ * @return {number}
+ */
+var percentageLetter = function(s, letter) {
+    let count=0;
+    for(let i=0;i<s.length;i++){
+        if(s[i]==letter){
+            count++;
+        }
+    }
+    return Math.floor((count*100)/s.length);
+    
+};
