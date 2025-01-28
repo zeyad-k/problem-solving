826387f16c4564ff72dfeee213fffcd61c944dc6
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2025-01-29 00:13:35 +0200

    [1] [Time Beats: 20.49%] [Memory Beats: 77.30%] - LeetPush

--- 1-Two-Sum.js
+++ 1-Two-Sum.js
@@ -4,10 +4,10 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    for (let i=0; i < nums.length; i++) {
-        for (let j=i+1; j < nums.length; j++) {
-            if ((target-nums[i] ) == nums[j]) return [i, j]
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if ((target - nums[i]) == nums[j]) return [i, j]
         }
     }
-    return[];
+    return [];
 };
